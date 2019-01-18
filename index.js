7df74065bf5d99f107bbcf1a8b5ccad146d726c6
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-18 20:39:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+
 function iterativeLog(array) {
   array.forEach((element, index) => {
-    console.log('${index}: ${element}')
+    console.log(`${index}: ${element}`)
   })
 }
-
 function iterate(callback) {
   let array = [1, 2, 3, 4]
   array.forEach(callback);
